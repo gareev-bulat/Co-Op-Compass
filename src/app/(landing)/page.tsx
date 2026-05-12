012c08a7a13eebc7ea65f0ceea96ade493cd7251
Bulat Gareev <gareevbulich63@gmail.com>
2026-05-12 18:30:59 -0400

    refactor: navigation to the dashboard

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,33 +1,39 @@
+"use client"
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function Home() {
   return (
-
     <div className="flex min-h-screen flex-col justify-center bg-[url('/bg.png')] bg-cover bg-center bg-no-repeat font-sans dark:bg-black">
-
       <nav className="absolute top-0 left-0 w-full flex items-center px-8 py-6">
-        <span className="text-brand-yellow font-bold text-xl">CO-OP COMPASS</span>
+        <span className="text-brand-yellow font-bold text-xl">
+          CO-OP COMPASS
+        </span>
       </nav>
 
-   
       <main className="text-white max-w-lg ml-30 -mt-30">
         <h1 className="text-6xl font-bold">Track. Analyze.</h1>
         <h1 className="text-6xl font-bold text-brand-yellow">Succeed.</h1>
         <p className="text-gray-300 mt-4 max-w-md text-lg">
-          The all-in-one platform for Drexel students to manage co-op applications and insights together.
+          The all-in-one platform for Drexel students to manage co-op
+          applications and insights together.
         </p>
 
         <div className="flex gap-4 mt-8">
-          <button className="bg-brand-yellow text-black font-semibold px-6 py-3 rounded-full">
-            Get Started
-          </button>
+          <Link href="/dashboard">
+            <button className="bg-brand-yellow text-black font-semibold px-6 py-3 rounded-full">
+              Get Started
+            </button>
+          </Link>
           <button className="border border-white text-white px-6 py-3 rounded-full">
             Learn More
           </button>
         </div>
 
         <p className="mt-8 text-gray-400 text-sm flex items-center gap-2">
-        Built for Drexel students. Always free.
+          Built for Drexel students. Always free.
         </p>
       </main>
     </div>
